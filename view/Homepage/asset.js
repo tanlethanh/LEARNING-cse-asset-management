425cf37366de8a8d1e5dd9f982f9a1df0c9c0480
nhatha32 <96714546+nhatha32@users.noreply.github.com>
2022-05-23 01:50:31 +0700

    Add files via upload

---
+++ view/Homepage/asset.js
@@ -0,0 +1,7 @@
+function openCloseChecklist() {
+    var element = document.getElementById("checkList");
+    if (element.style.display == "block")
+        element.style.display = "none";
+    else 
+      element.style.display = "block";
+}
