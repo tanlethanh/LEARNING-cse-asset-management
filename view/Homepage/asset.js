cdc14d1320b04ca0ea6f7bc0556e1b5cd54fcaff
nhatha32 <96714546+nhatha32@users.noreply.github.com>
2022-05-23 01:50:10 +0700

    Delete asset.js

--- view/Homepage/asset.js
+++
@@ -1,7 +0,0 @@
-function openCloseChecklist() {
-    var element = document.getElementById("checkList");
-    if (element.style.display == "none")
-        element.style.display = "block";
-    else 
-        element.style.display = "none";
-}
