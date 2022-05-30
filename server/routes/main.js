ffe490e335244e748b4938e16d53970a50db73eb
Lê Thanh Tân <tanlethanh8266@gmail.com>
2022-05-30 11:34:27 +0700

    clean

--- server/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const router = express.Router()
-
-const {getAllItem} = require('../controllers/main');
-
-router.get("/all-item", getAllItem)
-
-module.exports = router
