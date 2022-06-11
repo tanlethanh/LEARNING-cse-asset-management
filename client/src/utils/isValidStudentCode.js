8552ebd81b00fbadcdf35ae4af94f5e5b0ddf9fa
Lê Thanh Tân <tanlethanh8266@gmail.com>
2022-06-11 20:18:57 +0700

    fix validate student code

--- client/src/utils/isValidStudentCode.js
+++ client/src/utils/isValidStudentCode.js
@@ -3,6 +3,13 @@ export default function isValidStudentCode(code) {
     if (code.length !== 7) {
         return false
     }
+
+    for (let index = 0; index < code.length; index++) {
+        if (code.charAt(index) < '0' || code.charAt(index) > '9') {
+            return false
+        }
+    }
+
     const curYear = (new Date().getFullYear())
     const preFixCode = code.substring(0, 2)
     const year = 2000 + parseInt(preFixCode)
