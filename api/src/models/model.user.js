e8609fbee63b02a4d7006683b26c0f36137f840e
Lê Thanh Tân <tanlethanh8266@gmail.com>
2022-06-28 14:28:15 +0700

    api update - can edit user information

--- api/src/models/model.user.js
+++ api/src/models/model.user.js
@@ -16,28 +16,58 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         maxlength: 32,
-        trim: true
+        trim: true,
+        set: v => {
+            if (v === '') {
+                return undefined;
+            }
+            return v;
+        }
     },
     studentCode: {
         type: String,
         required: true,
         trim: true,
-        unique: true
+        unique: true,
+        set: v => {
+            if (v === '') {
+                return undefined;
+            }
+            return v;
+        }
     },
     phoneNumber: {
         type: String,
         require: true,
-        trim: true
+        trim: true,
+        set: v => {
+            if (v === '') {
+                return undefined;
+            }
+            return v;
+        }
     },
     email: {
         type: String,
         trim: true,
         required: true,
-        unique: true
+        unique: true,
+        set: v => {
+            if (v === '') {
+                return undefined;
+            }
+            return v;
+        }
     },
     hash_password: {
         type: String,
-        required: true
+        required: true,
+        set: v => {
+            if (v === '') {
+                return undefined;
+            }
+            return v;
+        }
     },
     basket: {
         id: mongoose.Schema.Types.ObjectId,
