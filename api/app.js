b5297e600c656a70ca1dd25f2a06d73e8c3be928
Lê Thanh Tân <tanlethanh8266@gmail.com>
2022-07-11 19:50:24 +0700

    fix router server

--- api/app.js
+++ api/app.js
@@ -71,7 +71,7 @@ app.use('/api/order', ordersRoute)
 
 // for client render
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
 })
 
 
